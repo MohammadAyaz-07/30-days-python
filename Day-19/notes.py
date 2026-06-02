9768639896e84cea221b788ff2cc6e6230c17e01
MohammadAyaz-07 <ayaz.bussiness7@gmail.com>
2026-06-02 21:45:51 +0530

    Practicing Day 19 file handling

---
+++ Day-19/notes.py
@@ -0,0 +1,11 @@
+# Day 19
+
+# File handling:
+'''
+File handling is an important part of programming which allows us to;
+create, read, update and delelte files.
+we use open() built-in function in py
+'''
+
+# syntax:
+open('filename',"mode")
