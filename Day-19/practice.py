091a4f842d8092408d333c8e8491f59667e6baa0
MohammadAyaz-07 <ayaz.bussiness7@gmail.com>
2026-06-04 21:53:23 +0530

    Done w day 19 notes lets solve the exercises tmr

--- Day-19/practice.py
+++ Day-19/practice.py
@@ -41,3 +41,62 @@ if os.path.exists("file3.txt"):
     os.remove("file3.txt")
 else:
     print("File doesnt exitst!")
+
+# File types :
+# dictionary
+ayaz_dict = {
+    "name" : "Ayaz",
+    "country" : "India",
+    "age" : 18,
+    "skills" : ["Python"]
+}
+
+# json : A string form of a dict
+
+# Changing JSON to Dict (we use loads method)
+ayaz_json = '''{
+    "name" : "Ayaz",
+    "country" : "India",
+    "age" : 18,
+    "skills" : ["Python"]
+}'''
+import json
+ayaz_json = '''{
+    "name" : "Ayaz",
+    "country" : "India",
+    "age" : 18,
+    "skills" : ["Python"]
+}'''
+
+ayazz_dict = json.loads(ayaz_json)
+print(type(ayazz_dict))
+print(ayazz_dict)
+print(ayazz_dict["name"])
+
+# Changing Dict to JSON (we use dumps method)
+ayazz_json = json.dumps(ayaz_json, indent=4) # indent could be 2,4,6,8.It beautifies the json
+print(type(ayazz_json))
+print(ayazz_json)
+
+with open("json_example.json", "w", encoding='utf-8') as f:
+    json.dump(ayazz_json, f, ensure_ascii=False, indent=4)
+
+
+# File w csv extension
+
+# ex for csv is:
+"name","country","age","skills"
+"Ayaz","India",18,["Python"]
+
+import csv
+with open("csv_example.csv", "r") as f:
+    csv_reader = csv.reader(f, delimiter=',') # we use , reader method to read csv
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f"Column names are: {", ".join(row)}")
+            line_count += 1
+        else:
+            print(f"\t{row[0]} is a Student. He lives in {row[1]}.")
+            line_count += 1
+    print(f"Number of lines = {line_count}")
