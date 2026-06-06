8dc10f75afa61c57cf3dc683ec6e3343d3fb2624
MohammadAyaz-07 <ayaz.bussiness7@gmail.com>
2026-06-06 23:33:12 +0530

    Solved Day 19 most spoken languages exercise

--- Day-19/exercise.py
+++ Day-19/exercise.py
@@ -24,3 +24,42 @@ no_of_line_and_words("obama.txt")
 no_of_line_and_words("michelle_obama_speech.txt")
 no_of_line_and_words("donald_speech.txt")
 no_of_line_and_words("melina_trump_speech.txt")
+
+'''
+2.Read the countries_data.json data file in data directory, create a function that finds the ten most spoken languages
+
+# Your output should look like this
+print(most_spoken_languages(filename='./data/countries_data.json', 10))
+[(91, 'English'),
+(45, 'French'),
+(25, 'Arabic'),
+(24, 'Spanish'),
+(9, 'Russian'),
+(9, 'Portuguese'),
+(8, 'Dutch'),
+(7, 'German'),
+(5, 'Chinese'),
+(4, 'Swahili'),
+(4, 'Serbian')]
+
+# Your output should look like this
+print(most_spoken_languages(filename='./data/countries_data.json', 3))
+[(91, 'English'),
+(45, 'French'),
+(25, 'Arabic')]
+'''
+import json
+
+def most_spoken_lang(filename,n):
+    with open(filename, encoding="utf-8") as f:
+        countries = json.load(f)
+        lang_count = {}
+        for country in countries:
+            for language in country["languages"]:
+                lang_count[language] = lang_count.get(language , 0) + 1
+        s = lang_count.items()
+        sorted_lang = sorted(s, key=lambda x: x[1], reverse=True)
+        return sorted_lang[:n]
+
+print(most_spoken_lang(filename="countries_data.json", n=5))
+
