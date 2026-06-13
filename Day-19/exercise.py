7c90f70bc07242b70325a55b96f124fbe09aafe2
MohammadAyaz-07 <ayaz.bussiness7@gmail.com>
2026-06-13 20:46:25 +0530

    Solved 2 more problmes from the ex-2

--- Day-19/exercise.py
+++ Day-19/exercise.py
@@ -105,3 +105,58 @@ def most_populated_countries(filename, n):
         sorted_ed = sorted(population_list, key=lambda x: x["population"], reverse=True)
         return sorted_ed[:n]
 print(most_populated_countries(filename="countries_data.json", n=10))
+
+'''
+Exercises: Level 2
+1. Extract all incoming email addresses as a list from the email_exchange_big.txt file.
+'''
+import re
+with open("email_exchange_big.txt") as f:
+    text = f.read()
+    pattern = re.findall(r"\b\w+[@]\w+[.]\w+\b", text, re.I )
+print(pattern)
+
+'''
+2. Find the most common words in the English language. Call the name of your function find_most_common_words,
+ it will take two parameters - a string or a file and a positive integer, indicating the number of words. 
+ Your function will return an array of tuples in descending order. Check the output
+
+    # Your output should look like this
+    print(find_most_common_words('sample.txt', 10))
+    [(10, 'the'),
+    (8, 'be'),
+    (6, 'to'),
+    (6, 'of'),
+    (5, 'and'),
+    (4, 'a'),
+    (4, 'in'),
+    (3, 'that'),
+    (2, 'have'),
+    (2, 'I')]
+
+    # Your output should look like this
+    print(find_most_common_words('sample.txt', 5))
+
+    [(10, 'the'),
+    (8, 'be'),
+    (6, 'to'),
+    (6, 'of'),
+    (5, 'and')]
+'''
+
+def find_most_common_words(filename, n):
+    with open(filename) as f:
+        text = f.read()
+        txt = re.findall("\w+", text, re.I)
+        count = {}
+        for w in txt:
+            count[w] = count.get(w, 0) + 1
+        c = count.items()
+        cc = sorted(c, key=lambda x: x[1], reverse=True)
+        r = []
+        for x in cc:
+            xx = (x[1], x[0])
+            r.append(xx)
+        return r[:n]
+print(find_most_common_words("sample.txt", 5))
+
